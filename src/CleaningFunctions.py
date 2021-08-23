776908a8a7ba2f530f886e22d95971939b4bd5e0
diegodiazgarciadev <diegodiazgarciadev@gmail.com>
2021-08-23 21:36:02 +0200

    some modifications

--- src/CleaningFunctions.py
+++ src/CleaningFunctions.py
@@ -1,11 +1,17 @@
-from geopy.geocoders import Nominatim
 import re
 
 
-# We need a function so that we will take only 3 values ( N,Y, Unknown)
-# We will use this function to return the correct Character for the Sex.
-# If the Character is not F or M we will return "Unknown"
+#We need a function so that we will return 3 values ( N,Y, Unknown)
 def get_correct_character(x):
+    """ We need a function so that we will take only 3 values ( N,Y, Unknown)
+            We will use this function to return the correct Character for the Sex.
+            If the Character is not F or M we will return "Unknown" 
+
+    parameters : str (Character that determine if there is or not a deadly attack (y/N)) 
+
+    
+    returns : str (Return only Values N, Y, or Unknown depending on the input string)
+    """
     if x == "N" or x == "N " or x == " N ":
         return "N"
     elif x == "Y" or x == "Y " or x == " Y ":
@@ -16,6 +22,13 @@ def get_correct_character(x):
 # We will use this function to return the correct Character for the Sex.
 # If the Character is not F or M we will return "Unknown"
 def get_correct_sex(x):
+    """ We will use this function to return the correct Character for the Sex. 
+
+    parameters : str (Character that determine the sex( M/F)) 
+
+    
+    returns : str (Return only Values M, F, or Unknown depending on the input string)
+    """
     if x == "M" or x == "M " or x == " M ":
         return "M"
     elif x == "F" or x == "F " or x == " F ":
@@ -27,6 +40,13 @@ def get_correct_sex(x):
 # We want to use 5 different kind of sharks (White,Tiger, Lemon, Bull and Reef)
 # So we will filter (using these functions an ReGex) the Species columns looking for this 5 species
 def kind_of_shark_pattern(shark_str, pattern):
+    """ We will use this function to return a string of the pattern we are sending on the string we are sending. 
+
+    parameters : str, str (string where we want to find our patter, the patter) 
+
+    
+    returns : str (Return the string with the pattern if it finds it and an empty string if not)
+    """
     kind_of_shark = re.findall(pattern, shark_str)
     if kind_of_shark:
         return kind_of_shark
